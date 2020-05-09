7ac9e16179aa650a3555821214bd7f0c233e0e87
David Evans <d.evans@apple.com>
2020-05-09 01:50:07 +0100

    Fix sanity

--- Tests/NIOIMAPTests/B2MV+Tests.swift
+++ Tests/NIOIMAPTests/B2MV+Tests.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by David Evans on 09/05/2020.
 //
@@ -8,63 +8,65 @@
 import Foundation
 import XCTest
 
-import NIOIMAPCore
-import NIOIMAP
 import NIO
+import NIOIMAP
+import NIOIMAPCore
 import NIOTestUtils
 
-final class B2MV_Tests: XCTestCase {
- 
-    
-    
-}
-
+final class B2MV_Tests: XCTestCase {}
 
 // MARK: - Command
-extension B2MV_Tests {
 
+extension B2MV_Tests {
     func testCommand() {
         let inoutPairs: [(String, [NIOIMAP.CommandStream])] = [
-            
             // MARK: Capability
+
             ("tag CAPABILITY" + CRLF, [.command(.init("tag", .capability))]),
-            
+
             // MARK: Noop
+
             ("tag NOOP" + CRLF, [.command(.init("tag", .noop))]),
-            
+
             // MARK: Logout
+
             ("tag LOGOUT" + CRLF, [.command(.init("tag", .logout))]),
-            
+
             // MARK: StartTLS
+
             ("tag STARTTLS" + CRLF, [.command(.init("tag", .starttls))]),
-            
+
             // MARK: Authenticate
+
             // this tests causes nothing but trouble
             // ("tag AUTHENTICATE PLAIN" + CRLF, [.command(.init("tag", .authenticate("PLAIN", nil, [])))]),
-            
+
             // MARK: Login
+
             (#"tag LOGIN "foo" "bar""# + CRLF, [.command(.init("tag", .login("foo", "bar")))]),
             ("tag LOGIN \"\" {0}\r\n" + CRLF, [.command(.init("tag", .login("", "")))]),
             (#"tag LOGIN "foo" "bar""# + CRLF, [.command(.init("tag", .login("foo", "bar")))]),
             (#"tag LOGIN foo bar"# + CRLF, [.command(.init("tag", .login("foo", "bar")))]),
-            
+
             // MARK: Select
+
             ("tag SELECT box1" + CRLF, [.command(.init("tag", .select(.init("box1"), [])))]),
             ("tag SELECT \"box2\"" + CRLF, [.command(.init("tag", .select(.init("box2"), [])))]),
             ("tag SELECT {4}\r\nbox3" + CRLF, [.command(.init("tag", .select(.init("box3"), [])))]),
             ("tag SELECT box4 (k1 1 k2 2)" + CRLF, [.command(.init("tag", .select(.init("box4"), [.name("k1", value: .simple(.sequence([1]))), .name("k2", value: .simple(.sequence([1])))])))]),
 
             // MARK: Examine
+
             ("tag EXAMINE box1" + CRLF, [.command(.init("tag", .examine(.init("box1"), [])))]),
             ("tag EXAMINE \"box2\"" + CRLF, [.command(.init("tag", .examine(.init("box2"), [])))]),
             ("tag EXAMINE {4}\r\nbox3" + CRLF, [.command(.init("tag", .examine(.init("box3"), [])))]),
             ("tag EXAMINE box4 (k3 1 k4 2)" + CRLF, [.command(.init("tag", .examine(.init("box1"), [.name("k3", value: .simple(.sequence([1]))), .name("k4", value: .simple(.sequence([1])))])))]),
-            
+
             // MARK: Rename
+
             (#"tag RENAME "foo" "bar""# + CRLF, [.command(NIOIMAP.TaggedCommand("tag", .rename(from: NIOIMAP.MailboxName("foo"), to: NIOIMAP.MailboxName("bar"), params: [])))]),
             (#"tag RENAME InBoX "inBOX""# + CRLF, [.command(NIOIMAP.TaggedCommand("tag", .rename(from: .inbox, to: .inbox, params: [])))]),
             ("tag RENAME {1}\r\n1 {1}\r\n2" + CRLF, [.command(NIOIMAP.TaggedCommand("tag", .rename(from: NIOIMAP.MailboxName("1"), to: NIOIMAP.MailboxName("2"), params: [])))]),
-            
         ]
         do {
             try ByteToMessageDecoderVerifier.verifyDecoder(
@@ -96,14 +98,10 @@ extension B2MV_Tests {
             XCTFail("unhandled error: \(error)")
         }
     }
-    
 }
 
 // MARK: - Response
+
 extension B2MV_Tests {
-    
-    func testResponse() {
-        
-    }
-    
+    func testResponse() {}
 }
