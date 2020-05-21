4dd53d0030f2126c46d5fa8135f2075d87493a9c
David Evans <d.evans@apple.com>
2020-05-21 10:40:43 +0100

    Refactor into less files

---
+++ Sources/NIOIMAPCore/Grammar/Mailbox/MailboxInfo.swift
@@ -0,0 +1,129 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import struct NIO.ByteBuffer
+
+/// IMAPv4 `mailbox-list`
+public struct MailboxInfo: Equatable {
+    public var flags: Flags?
+    public var char: Character?
+    public var mailbox: MailboxName
+    public var listExtended: [MailboxName.ListExtendedItem]
+
+    public init(flags: Flags? = nil, char: Character? = nil, mailbox: MailboxName, listExtended: [MailboxName.ListExtendedItem]) {
+        self.flags = flags
+        self.char = char
+        self.mailbox = mailbox
+        self.listExtended = listExtended
+    }
+}
+
+// MARK: - Types
+
+extension MailboxInfo {
+    
+    /// IMAPv4 `mbx-list-sflag`
+    public enum SFlag: String, Equatable {
+        case noSelect = #"\Noselect"#
+        case marked = #"\Marked"#
+        case unmarked = #"\Unmarked"#
+        case nonExistent = #"\Nonexistent"#
+
+        public init?(rawValue: String) {
+            switch rawValue.lowercased() {
+            case #"\noselect"#:
+                self = .noSelect
+            case #"\marked"#:
+                self = .marked
+            case #"\unmarked"#:
+                self = .unmarked
+            case #"\nonexistent"#:
+                self = .nonExistent
+            default:
+                return nil
+            }
+        }
+    }
+    
+    /// IMAPv4 `mbx-list-oflag`
+    public enum OFlag: Equatable {
+        case noInferiors
+        case subscribed
+        case remote
+        case child(ChildMailboxFlag)
+        case other(String)
+    }
+    
+    /// IMAPv4 `mbx-list-flags`
+    public struct Flags: Equatable {
+        public var oFlags: [OFlag]
+        public var sFlag: SFlag?
+
+        public init(oFlags: [MailboxInfo.OFlag], sFlag: MailboxInfo.SFlag? = nil) {
+            self.oFlags = oFlags
+            self.sFlag = sFlag
+        }
+    }
+    
+}
+
+// MARK: - Encoding
+
+extension EncodeBuffer {
+    @discardableResult mutating func writeMailboxInfo(_ list: MailboxInfo) -> Int {
+        self.writeString("(") +
+            self.writeIfExists(list.flags) { (flags) -> Int in
+                self.writeMailboxListFlags(flags)
+            } +
+            self.writeString(") ") +
+            self.writeIfExists(list.char) { (char) -> Int in
+                self.writeString("\(char) ")
+            } +
+            self.writeMailbox(list.mailbox)
+    }
+    
+    @discardableResult mutating func writeMailboxListSFlag(_ flag: MailboxInfo.SFlag) -> Int {
+        self.writeString(flag.rawValue)
+    }
+    
+    @discardableResult mutating func writeMailboxListOFlag(_ flag: MailboxInfo.OFlag) -> Int {
+        switch flag {
+        case .noInferiors:
+            return self.writeString(#"\Noinferiors"#)
+        case .subscribed:
+            return self.writeString(#"\Subscribed"#)
+        case .remote:
+            return self.writeString(#"\Remote"#)
+        case .child(let child):
+            return self.writeChildMailboxFlag(child)
+        case .other(let string):
+            return self.writeString("\\\(string)")
+        }
+    }
+    
+    @discardableResult mutating func writeMailboxListFlags(_ flags: MailboxInfo.Flags) -> Int {
+        if let sFlag = flags.sFlag {
+            return
+                self.writeMailboxListSFlag(sFlag) +
+                self.writeArray(flags.oFlags, separator: "", parenthesis: false) { (flag, self) in
+                    self.writeSpace() +
+                        self.writeMailboxListOFlag(flag)
+                }
+        } else {
+            return self.writeArray(flags.oFlags, parenthesis: false) { (element, self) in
+                self.writeMailboxListOFlag(element)
+            }
+        }
+    }
+}
