5e50102486d2cd0bc4f405a9b33ef4dbd98196fc
David Evans <d.evans@apple.com>
2020-06-01 11:33:37 +0100

    Fix sanity

--- Sources/NIOIMAPCore/Grammar/Capability.swift
+++ Sources/NIOIMAPCore/Grammar/Capability.swift
@@ -15,32 +15,30 @@
 import struct NIO.ByteBuffer
 
 struct EncodingOptions: OptionSet {
+    static let move = EncodingOptions(rawValue: 1 << 0)
+    static let namespace = EncodingOptions(rawValue: 1 << 1)
+    static let id = EncodingOptions(rawValue: 1 << 2)
+    static let binary = EncodingOptions(rawValue: 1 << 3)
 
-    static let move         = EncodingOptions(rawValue: 1 << 0)
-    static let namespace    = EncodingOptions(rawValue: 1 << 1)
-    static let id           = EncodingOptions(rawValue: 1 << 2)
-    static let binary       = EncodingOptions(rawValue: 1 << 3)
-    
     var rawValue: Int
 
     init(rawValue: Int) {
         self.rawValue = rawValue
     }
-    
+
     init(capabilities: [Capability]) {
         self = .init(rawValue: 0)
-        
+
         let inputs: [(Capability, EncodingOptions)] = [
             (.move, .move),
             (.id, .id),
             (.namespace, .namespace),
-            (.binary, .binary)
+            (.binary, .binary),
         ]
         for (strCap, cap) in inputs where capabilities.contains(strCap) {
             self.insert(cap)
         }
     }
-    
 }
 
 /// IMAPv4 `capability`
