f9a0f966c293e5a7a0f0ddeac6ac448138239c74
David Evans <d.evans@apple.com>
2020-07-15 09:44:40 +0100

    Support LITERAL-

--- Tests/NIOIMAPCoreTests/Grammar/EncodeTestClass.swift
+++ Tests/NIOIMAPCoreTests/Grammar/EncodeTestClass.swift
@@ -79,7 +79,7 @@ extension CommandEncodingOptions {
     static var rfc3501: CommandEncodingOptions { CommandEncodingOptions() }
     static var literalPlus: CommandEncodingOptions {
         var o = CommandEncodingOptions()
-        o.useNonSynchronizingLiteral = true
+        o.useNonSynchronizingLiteralPlus = true
         return o
     }
 
