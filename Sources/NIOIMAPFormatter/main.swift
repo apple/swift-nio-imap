2a9faa0c66b0c6368490e6076901ec717eef3987
David Evans <d.evans@apple.com>
2020-09-08 11:18:07 +0100

    Silence warnings

--- Sources/NIOIMAPFormatter/main.swift
+++ Sources/NIOIMAPFormatter/main.swift
@@ -22,9 +22,9 @@ import Glibc
 #if os(Linux) || os(macOS)
 let filePath: String
 #if compiler(>=5.3)
-filePath = #file
+filePath = (#file)
 #else
-filePath = #file
+filePath = (#file)
 #endif
 let swiftFormat = URL(fileURLWithPath: CommandLine.arguments.first!)
     .deletingLastPathComponent()
