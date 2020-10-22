fa291d69d66da1db55f7bb8c8b88ee59d91ed40e
David Evans <d.evans@apple.com>
2020-10-22 12:41:15 +0100

    Remove link to NIO http 2

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -80,7 +80,6 @@ cat >> "$module_switcher" <<"EOF"
 For the API documentation of the other repositories in the SwiftNIO family check:
 - [`swift-nio` API docs](https://apple.github.io/swift-nio/docs/current/NIO/index.html)
 - [`swift-nio-ssl` API docs](https://apple.github.io/swift-nio-ssl/docs/current/NIOSSL/index.html)
-- [`swift-nio-http2` API docs](https://apple.github.io/swift-nio-http2/docs/current/NIOHTTP2/index.html)
 EOF
 
 for module in "${modules[@]}"; do
