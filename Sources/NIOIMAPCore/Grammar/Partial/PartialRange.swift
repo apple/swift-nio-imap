4eddfeaa36adb5cba4643a8a41a5483bac546456
David Evans <d.evans@apple.com>
2020-12-03 14:35:26 +0000

    Document PartialRange

--- Sources/NIOIMAPCore/Grammar/Partial/PartialRange.swift
+++ Sources/NIOIMAPCore/Grammar/Partial/PartialRange.swift
@@ -12,11 +12,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-/// See RFC 5092
+/// Represents a range of bytes in a larger whole. See RFC 5092
 public struct PartialRange: Equatable {
+    
+    /// The offset in bytes from the beginning of the message/data in question.
     public var offset: Int
+    
+    /// The number of bytes the range covers.
     public var length: Int?
 
+    /// Creates a new `PartialRange`
+    /// - parameter offset: The offset in bytes from the beginning of the message/data in question.
+    /// - parameter length: The number of bytes the range covers.
     public init(offset: Int, length: Int?) {
         self.offset = offset
         self.length = length
