f794fc4283056b4ead11c1998da401990efa47e5
David Evans <d.evans@apple.com>
2020-12-08 17:22:17 +0000

    Document UseAttribute

--- Sources/NIOIMAPCore/Grammar/UseAttribute.swift
+++ Sources/NIOIMAPCore/Grammar/UseAttribute.swift
@@ -14,19 +14,38 @@
 
 import struct NIO.ByteBuffer
 
+/// A `UseAttribute` is  a special-use attribute as defined in RFC 6154.
+/// They're used to designate a special use to certain mailboxes.
 public struct UseAttribute: Equatable, RawRepresentable {
     public typealias RawValue = String
 
+    /// A mailbox that presents all messages in the user's store.
     public static var all = Self(rawValue: "\\All")
+    
+    /// Used to archive messages - note that the meaning of "archived" will vary from server to server.
     public static var archive = Self(rawValue: "\\Archive")
+    
+    /// Used to store draft messages that have not been sent.
     public static var drafts = Self(rawValue: "\\Drafts")
+    
+    /// Stores messages that have been marked as "important" for some reason.
     public static var flagged = Self(rawValue: "\\Flagged")
+    
+    /// Stores messages deemed to be spam of junk mail, e.g. from a mailing list.
     public static var junk = Self(rawValue: "\\Junk")
+    
+    /// Holds copies of messages that have been sent.
     public static var sent = Self(rawValue: "\\Sent")
+    
+    /// Holds messages that have been deleted or marked for deletion.
     public static var trash = Self(rawValue: "\\Trash")
 
+    /// The raw value of the attribute, e.g. `\\Trash`.
     public var rawValue: String
 
+    // TODO: Make this case-insensitive
+    /// Creates a new `UseAttribute` from the raw `String`. Note that
+    /// usually it should be sufficient to just use the predefined attributes, e.g. `.drafts`.
     public init(rawValue: String) {
         self.rawValue = rawValue
     }
