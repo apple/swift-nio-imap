61fee7c66cba97a046aea0b2195def72d9dce506
David Evans <d.evans@apple.com>
2020-12-08 23:31:29 +0000

    Document ResponseEncodingOptions

--- Sources/NIOIMAPCore/ResponseEncodingOptions.swift
+++ Sources/NIOIMAPCore/ResponseEncodingOptions.swift
@@ -12,6 +12,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+/// Configuration options for writing Responses to a buffer for transmission.
 public struct ResponseEncodingOptions: Equatable {
     /// Use RFC 3501 _quoted strings_ when possible (and the string is relatively short).
     public var useQuotedString: Bool
