046cb4b8476c5bd07bc0cbd3cb9f602bc5fb53a1
David Evans <d.evans@apple.com>
2020-12-09 18:18:32 +0000

    Document UntaggedStatus

--- Sources/NIOIMAPCore/Grammar/Response/UntaggedResponseState.swift
+++ Sources/NIOIMAPCore/Grammar/Response/UntaggedResponseState.swift
@@ -18,10 +18,34 @@ import struct NIO.ByteBuffer
 ///
 /// The untagged versions in RFC 3501 section 7.1
 public enum UntaggedStatus: Equatable {
+    
+    /// Indicates a success.
     case ok(ResponseText)
+    
+    /// Indicates an operational error message from the
+    /// server.  When tagged, it indicates unsuccessful completion of the
+    /// associated command.  The untagged form indicates a warning; the
+    /// command can still complete successfully.  The human-readable text
+    /// describes the condition.
     case no(ResponseText)
+    
+    /// The BAD response indicates an error message from the server.  When
+    /// tagged, it reports a protocol-level error in the client's command;
+    /// the tag indicates the command that caused the error.  The untagged
+    /// form indicates a protocol-level error for which the associated
+    /// command can not be determined; it can also indicate an internal
+    /// server failure.  The human-readable text describes the condition.
     case bad(ResponseText)
+    
+    /// The PREAUTH response is always untagged, and is one of three
+    /// possible greetings at connection startup.  It indicates that the
+    /// connection has already been authenticated by external means; thus
+    /// no LOGIN command is needed.
     case preauth(ResponseText)
+    
+    /// The BYE response is always untagged, and indicates that the server
+    /// is about to close the connection.  The human-readable text MAY be
+    /// displayed to the user in a status report by the client.
     case bye(ResponseText)
 }
 
