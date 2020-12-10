309fa0d08b80ed1c4f9d44506e1f813efb7ba49d
David Evans <d.evans@apple.com>
2020-12-10 00:50:53 +0000

    Document IURLAuthRump

--- Sources/NIOIMAPCore/Grammar/IUrlAuth.swift
+++ Sources/NIOIMAPCore/Grammar/IUrlAuth.swift
@@ -12,11 +12,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-/// RFC 5092
+/// Specifies a URL and a verifier that can be used to verify the authorisation URL.
 public struct IURLAuth: Equatable {
+    
+    /// The URL.
     public var auth: IURLAuthRump
+    
+    /// The auth url verifier.
     public var verifier: IUAVerifier
 
+    /// Creates a new `IURLAuth`.
+    /// - parameter auth: The auth URL.
+    /// - parameter verifier: The auth URL verifier.
     public init(auth: IURLAuthRump, verifier: IUAVerifier) {
         self.auth = auth
         self.verifier = verifier
