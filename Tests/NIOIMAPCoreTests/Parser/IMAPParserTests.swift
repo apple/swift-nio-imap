d902a22d5ea2c03904973270d43fac64a691198d
David Evans <d.evans@apple.com>
2021-01-19 13:00:42 +0000

    Extract parse command

--- Tests/NIOIMAPCoreTests/Parser/IMAPParserTests.swift
+++ Tests/NIOIMAPCoreTests/Parser/IMAPParserTests.swift
@@ -27,9 +27,13 @@ let CR = UInt8(ascii: "\r")
 let LF = UInt8(ascii: "\n")
 let CRLF = String(decoding: [CR, LF], as: Unicode.UTF8.self)
 
-final class ParserUnitTests: XCTestCase {}
+protocol _ParserTestHelpers {
+    
+}
 
-extension ParserUnitTests {
+final class ParserUnitTests: XCTestCase, _ParserTestHelpers {}
+
+extension _ParserTestHelpers {
     private func iterateTestInputs_generic<T: Equatable>(_ inputs: [(String, String, T, UInt)], testFunction: (inout ByteBuffer, StackTracker) throws -> T) {
         for (input, terminator, expected, line) in inputs {
             TestUtilities.withBuffer(input, terminator: terminator, shouldRemainUnchanged: false, file: (#file), line: line) { (buffer) in
@@ -92,7 +96,7 @@ extension ParserUnitTests {
     /// - parameter validInputs: An array of (Input, Terminator, ExectedResult, Line). These inputs should succeed.
     /// - parameter parserErrorInputs: An array of (Input, Terminator, ExectedResult, Line). These inputs should fail by throwing a `ParserError`.
     /// - parameter incompleteMessageInputs: An array of (Input, Terminator, ExectedResult, Line). These inputs should fail by throwing an `_IncompleteMessage`.
-    fileprivate func iterateTests<T: Equatable>(
+    func iterateTests<T: Equatable>(
         testFunction: (inout ByteBuffer, StackTracker) throws -> T,
         validInputs: [(String, String, T, UInt)],
         parserErrorInputs: [(String, String, UInt)],
@@ -108,7 +112,7 @@ extension ParserUnitTests {
     /// - parameter validInputs: An array of (Input, Terminator, ExectedResult, Line). These inputs should succeed.
     /// - parameter parserErrorInputs: An array of (Input, Terminator, ExectedResult, Line). These inputs should fail by throwing a `ParserError`.
     /// - parameter incompleteMessageInputs: An array of (Input, Terminator, ExectedResult, Line). These inputs should fail by throwing an `_IncompleteMessage`.
-    fileprivate func iterateTests(
+    func iterateTests(
         testFunction: (inout ByteBuffer, StackTracker) throws -> Void,
         validInputs: [(String, String, UInt)],
         parserErrorInputs: [(String, String, UInt)],
@@ -1031,122 +1035,6 @@ extension ParserUnitTests {
                 ("\\Sent", "", .sent, #line),
                 ("\\Drafts", "", .drafts, #line),
                 ("\\Other", " ", .init("\\Other"), #line),
-            ],
-            parserErrorInputs: [],
-            incompleteMessageInputs: []
-        )
-    }
-}
-
-// MARK: - parseCommand
-
-extension ParserUnitTests {
-    func testParseCommand_valid_any() {
-        TestUtilities.withBuffer("a1 NOOP", terminator: "\r\n") { (buffer) in
-            let result = try GrammarParser.parseCommand(buffer: &buffer, tracker: .testTracker)
-            XCTAssertEqual(result.tag, "a1")
-            XCTAssertEqual(result.command, .noop)
-        }
-    }
-
-    func testParseCommand_valid_auth() {
-        TestUtilities.withBuffer("a1 CREATE \"mailbox\"", terminator: "\r\n") { (buffer) in
-            let result = try GrammarParser.parseCommand(buffer: &buffer, tracker: .testTracker)
-            XCTAssertEqual(result.tag, "a1")
-            XCTAssertEqual(result.command, .create(MailboxName("mailbox"), []))
-        }
-    }
-
-    func testParseCommand_valid_nonauth() {
-        TestUtilities.withBuffer("a1 STARTTLS", terminator: "\r\n") { (buffer) in
-            let result = try GrammarParser.parseCommand(buffer: &buffer, tracker: .testTracker)
-            XCTAssertEqual(result.tag, "a1")
-            XCTAssertEqual(result.command, .starttls)
-        }
-    }
-
-    func testParseCommand_valid_select() {
-        TestUtilities.withBuffer("a1 CHECK", terminator: "\r\n") { (buffer) in
-            let result = try GrammarParser.parseCommand(buffer: &buffer, tracker: .testTracker)
-            XCTAssertEqual(result.tag, "a1")
-            XCTAssertEqual(result.command, .check)
-        }
-    }
-}
-
-// MARK: - CommandType parseCommandAny
-
-extension ParserUnitTests {
-    func testParseCommandAny() {
-        self.iterateTests(
-            testFunction: GrammarParser.parseCommandAny,
-            validInputs: [
-                ("CAPABILITY", " ", .capability, #line),
-                ("LOGOUT", " ", .logout, #line),
-                ("NOOP", " ", .noop, #line),
-            ],
-            parserErrorInputs: [],
-            incompleteMessageInputs: []
-        )
-    }
-}
-
-// MARK: - CommandType parseCommandNonAuth
-
-extension ParserUnitTests {
-    func testParseCommandNonAuth() {
-        self.iterateTests(
-            testFunction: GrammarParser.parseCommandNonauth,
-            validInputs: [
-                ("LOGIN david evans", "\r\n", .login(username: "david", password: "evans"), #line),
-                ("AUTHENTICATE some", "\r\n", .authenticate(method: "some", initialClientResponse: nil), #line),
-                ("AUTHENTICATE some =", "\r\n", .authenticate(method: "some", initialClientResponse: .empty), #line),
-                ("STARTTLS", "\r\n", .starttls, #line),
-            ],
-            parserErrorInputs: [],
-            incompleteMessageInputs: []
-        )
-    }
-}
-
-// MARK: - CommandType parseCommandAuth
-
-extension ParserUnitTests {
-    func testParseCommandAuth() {
-        self.iterateTests(
-            testFunction: GrammarParser.parseCommandAuth,
-            validInputs: [
-                ("LSUB inbox someList", " ", .lsub(reference: .inbox, pattern: "someList"), #line),
-                ("CREATE inbox (something)", " ", .create(.inbox, [.labelled(.init(name: "something", value: nil))]), #line),
-                ("NAMESPACE", " ", .namespace, #line),
-                ("GETMETADATA INBOX a", " ", .getMetadata(options: [], mailbox: .inbox, entries: ["a"]), #line),
-                ("GETMETADATA (MAXSIZE 123) INBOX (a b)", " ", .getMetadata(options: [.maxSize(123)], mailbox: .inbox, entries: ["a", "b"]), #line),
-                ("SETMETADATA INBOX (a NIL)", " ", .setMetadata(mailbox: .inbox, entries: [.init(name: "a", value: .init(nil))]), #line),
-                ("RESETKEY", "\r", .resetKey(mailbox: nil, mechanisms: []), #line),
-                ("RESETKEY INBOX", "\r", .resetKey(mailbox: .inbox, mechanisms: []), #line),
-                ("RESETKEY INBOX INTERNAL", "\r", .resetKey(mailbox: .inbox, mechanisms: [.internal]), #line),
-                ("RESETKEY INBOX INTERNAL test", "\r", .resetKey(mailbox: .inbox, mechanisms: [.internal, .init("test")]), #line),
-                ("GENURLAUTH test INTERNAL", "\r", .genURLAuth([.init(urlRump: "test", mechanism: .internal)]), #line),
-                ("GENURLAUTH test INTERNAL test2 INTERNAL", "\r", .genURLAuth([.init(urlRump: "test", mechanism: .internal), .init(urlRump: "test2", mechanism: .internal)]), #line),
-                ("URLFETCH test", "\r", .urlFetch(["test"]), #line),
-                ("URLFETCH test1 test2", "\r", .urlFetch(["test1", "test2"]), #line),
-            ],
-            parserErrorInputs: [],
-            incompleteMessageInputs: []
-        )
-    }
-}
-
-// MARK: - CommandType parseCommandSelect
-
-extension ParserUnitTests {
-    func testParseCommandSelect() {
-        self.iterateTests(
-            testFunction: GrammarParser.parseCommandSelect,
-            validInputs: [
-                ("UNSELECT", " ", .unselect, #line),
-                ("unselect", " ", .unselect, #line),
-                ("UNSelect", " ", .unselect, #line),
             ],
             parserErrorInputs: [],
             incompleteMessageInputs: []
