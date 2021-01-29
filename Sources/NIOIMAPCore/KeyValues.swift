4bc1506506f0d4bc659d7f6ccdf93a3204783d89
David Evans <d.evans@apple.com>
2021-01-29 18:52:24 +0000

    Converting backing to KeyValue struct

--- Sources/NIOIMAPCore/KeyValues.swift
+++ Sources/NIOIMAPCore/KeyValues.swift
@@ -13,44 +13,40 @@
 //===----------------------------------------------------------------------===//
 
 /// A dictionary-like structure that preserves insert order, and provides O(n) lookup based on some `Key`
-public struct KeyValues<Key: Hashable, Value: Hashable> {
-    var _backing: [(Key, Value)]
+public struct KeyValues<Key: Hashable, Value: Hashable>: Hashable {
+    
+    @usableFromInline struct KeyValue<Key: Hashable, Value: Hashable>: Hashable {
+        @usableFromInline var key: Key
+        @usableFromInline var value: Value
+        @usableFromInline init(key: Key, value: Value) {
+            self.key = key
+            self.value = value
+        }
+    }
+
+    @usableFromInline
+    var _backing: [KeyValue<Key, Value>]
 
     /// The number of key/value pairs in the collection.
+    @inlinable
     public var count: Int { self._backing.count }
 
     /// Creates a new `KeyValues` from an array of key/value tuples.
+    @inlinable
     public init(_ array: [(Key, Value)] = []) {
-        self._backing = array
+        self._backing = array.map { KeyValue(key: $0.0, value: $0.1) }
     }
 
     /// Creates a new `KeyValues` from a dictionary,
+    @inlinable
     public init(_ dic: [Key: Value]) {
-        self._backing = dic.map { ($0.key, $0.value) }
+        self._backing = dic.map { KeyValue(key: $0.key, value: $0.value) }
     }
 
     /// Appends an element to the collection
+    @inlinable
     public mutating func append(_ pair: (Key, Value)) {
-        self._backing.append(pair)
-    }
-}
-
-// MARK: - Hashable
-
-extension KeyValues: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        for (key, value) in self._backing {
-            hasher.combine(key)
-            hasher.combine(value)
-        }
-    }
-}
-
-// MARK: - Eqautable
-
-extension KeyValues: Equatable {
-    public static func == (lhs: KeyValues<Key, Value>, rhs: KeyValues<Key, Value>) -> Bool {
-        lhs._backing.map { $0.0 } == rhs._backing.map { $0.0 } && lhs._backing.map { $0.1 } == rhs._backing.map { $0.1 }
+        self._backing.append(KeyValue(key: pair.0, value: pair.1))
     }
 }
 
@@ -61,27 +57,47 @@ extension KeyValues: ExpressibleByDictionaryLiteral {
 
     public typealias Value = Value
 
+    @inlinable
     public init(dictionaryLiteral elements: (Key, Value)...) {
-        self._backing = elements
+        self._backing = elements.map { KeyValue(key: $0.0, value: $0.1) }
     }
 }
 
 // MARK: - Subscripting
 
 extension KeyValues {
-    subscript(index: Key) -> Value? {
-        self._backing.first(where: { $0.0 == index })?.1
+    @inlinable
+    subscript(key: Key) -> Value? {
+        self._backing.first(where: { $0.key == key })?.value
     }
 }
 
 // MARK: - Sequence
 
-extension KeyValues: Sequence {
-    public typealias Iterator = IndexingIterator<[(Key, Value)]>
-
-    public typealias Element = (Key, Value)
-
-    public func makeIterator() -> IndexingIterator<[(Key, Value)]> {
-        self._backing.makeIterator()
+extension KeyValues: Collection {
+    
+    public struct Index: Comparable {
+        public static func < (lhs: KeyValues.Index, rhs: KeyValues.Index) -> Bool {
+            return lhs.index < rhs.index
+        }
+        internal var index: Int
     }
+
+
+    public func index(after i: Index) -> Index {
+        return Index(index: self._backing.index(after: i.index))
+    }
+    
+    public subscript(position: Index) -> (Key, Value) {
+        return (self._backing[position.index].key, self._backing[position.index].value)
+    }
+    
+    public var startIndex: Index {
+        return Index(index: self._backing.startIndex)
+    }
+    
+    public var endIndex: Index {
+        return Index(index: self._backing.endIndex)
+    }
+    
 }
