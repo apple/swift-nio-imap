f7c069a1b12991bed9ca4fe0a37f4cec9be176e5
David Evans <d.evans@apple.com>
2021-02-01 13:26:59 +0000

    Remove hasNextChunk

--- Sources/NIOIMAPCore/EncodeBuffer.swift
+++ Sources/NIOIMAPCore/EncodeBuffer.swift
@@ -73,7 +73,6 @@ extension EncodeBuffer {
 }
 
 extension EncodeBuffer {
-    public var hasNextChunk: Bool { self._buffer.readableBytes > 0 }
 
     /// Represents a piece of data that is ready to be written to the network.
     public struct Chunk {
