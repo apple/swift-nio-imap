433c974e9916d96d14774dfee48037f97a2717e2
David Evans <d.evans@apple.com>
2021-03-03 20:57:53 +0000

    Soundness

--- Sources/NIOIMAPCore/Grammar/LastCommandSet.swift
+++ Sources/NIOIMAPCore/Grammar/LastCommandSet.swift
@@ -16,26 +16,21 @@
 /// protocol. It is used to provide an interface dictating how the generic type is written to a
 /// `EncodeBuffer`.
 public protocol LastCommandSetProtocol: Hashable {
- 
     /// Writes the set to an `inout EncodeBuffer`.
     func writeIntoBuffer(_ buffer: inout EncodeBuffer) -> Int
-    
 }
 
 /// Provides support for using either the result of the last command (`.lastCommand`) or
 /// a concrete set type.
 public enum LastCommandSet<T: LastCommandSetProtocol>: Hashable {
-    
     /// A specific set that will be sent to the IMAP server. E.g. `1, 2:5, 10:*`
     case set(T)
-    
+
     /// Tells the server to use the result of the last command, described in RFC 5182.
     case lastCommand
-    
 }
 
 extension EncodeBuffer {
-    
     @discardableResult mutating func writeLastCommandSet<T>(_ set: LastCommandSet<T>) -> Int {
         switch set {
         case .lastCommand:
@@ -44,5 +39,4 @@ extension EncodeBuffer {
             return set.writeIntoBuffer(&self)
         }
     }
-    
 }
--- Sources/NIOIMAPCore/Grammar/Sequence/SequenceRangeSet.swift
+++ Sources/NIOIMAPCore/Grammar/Sequence/SequenceRangeSet.swift
@@ -247,11 +247,9 @@ extension SequenceRangeSet: SetAlgebra {
 // MARK: - Encoding
 
 extension SequenceRangeSet: LastCommandSetProtocol {
-    
     public func writeIntoBuffer(_ buffer: inout EncodeBuffer) -> Int {
         buffer.writeArray(self.ranges.ranges, separator: ",", parenthesis: false) { (element, buffer) in
             buffer.writeSequenceRange(SequenceRange(element))
         }
     }
-    
 }
