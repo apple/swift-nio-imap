26a95e3944a11ad0b842b19a2faee48bb818d71f
David Evans <d.evans@apple.com>
2021-03-16 12:14:02 +0000

    Support parsing an extra space

--- Sources/NIOIMAPCore/Parser/Grammar/GrammarParser.swift
+++ Sources/NIOIMAPCore/Parser/Grammar/GrammarParser.swift
@@ -597,6 +597,7 @@ extension GrammarParser {
                 }
                 return output
             } ?? []
+            try ParserLibrary.optional(buffer: &buffer, tracker: tracker, parser: ParserLibrary.space)
             try ParserLibrary.fixedString(")", buffer: &buffer, tracker: tracker)
             return flags
         }
