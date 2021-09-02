974bc099e20fd70dbd9da07ee4c98135f5e6aca5
David Evans <d.evans@apple.com>
2021-09-02 21:54:19 +0100

    Add extremely simple tests cases

--- Sources/NIOIMAP/Client/ClientFramingParser.swift
+++ Sources/NIOIMAP/Client/ClientFramingParser.swift
@@ -17,10 +17,18 @@ import NIO
 struct ClientFramingParser: Hashable {
     
     enum State: Hashable {
-        
+        case normalTraversal
+        case foundCR
+        case foundLF
     }
     
+    var state: State = .normalTraversal
+    var frameLength: Int = 0
     var buffer = ByteBuffer()
+    
+    init() {
+        
+    }
     
     mutating func appendAndFrameBuffer(_ buffer: inout ByteBuffer) -> [ByteBuffer] {
         
@@ -30,12 +38,83 @@ struct ClientFramingParser: Hashable {
         }
         
         self.buffer.writeBuffer(&buffer)
-        return self.parseFrame()
+        return self.parseFrames()
     }
     
-    private mutating func parseFrame() -> [ByteBuffer] {
+    private mutating func parseFrames() -> [ByteBuffer] {
         assert(self.buffer.readableBytes > 0)
         
-        return []
+        var results: [ByteBuffer] = []
+        while let frame = self.parseFrame() {
+            results.append(frame)
+        }
+        return results
     }
+    
+    private mutating func parseFrame() -> ByteBuffer? {
+        var foundFrame = false
+        while self.frameLength < self.buffer.readableBytes && !foundFrame {
+            switch self.state {
+                
+            case .normalTraversal:
+                self.readByte_state_normalTraversal()
+                
+            case .foundCR:
+                self.readByte_state_foundCR()
+                self.state = .normalTraversal
+                foundFrame = true
+                
+            case .foundLF:
+                self.state = .normalTraversal
+                foundFrame = true
+            }
+        }
+        
+        if foundFrame {
+            defer {
+                self.frameLength = 0
+            }
+            return self.buffer.readSlice(length: self.frameLength)
+        }
+        
+        return nil
+    }
+    
+    private mutating func readByte() -> UInt8 {
+        assert(self.buffer.readableBytes > 0)
+        assert(self.frameLength < self.buffer.readableBytes)
+        defer {
+            self.frameLength += 1
+        }
+        return self.buffer.getInteger(at: self.buffer.readerIndex + self.frameLength)! // we've asserted this is ok
+    }
+}
+
+extension ClientFramingParser {
+    
+    private mutating func readByte_state_normalTraversal() {
+        let byte = self.readByte()
+        switch byte {
+        case UInt8(ascii: "\r"):
+            self.state = .foundCR
+        case UInt8(ascii: "\n"):
+            self.state = .foundLF
+        case UInt8(ascii: "{"):
+            break
+        default:
+            break
+        }
+    }
+    
+    private mutating func readByte_state_foundCR() {
+        // We've found the end of a frame here.
+        // If the next byte is an LF then we need to also consume
+        // that, otherwise consider go back a byte and consider
+        // that to be the end of the frame
+        let byte = self.readByte()
+        if byte != UInt8(ascii: "\n") {
+            self.frameLength -= 1
+        }
+    }
+    
 }
--- Tests/NIOIMAPTests/FramingParserTests.swift
+++ Tests/NIOIMAPTests/FramingParserTests.swift
@@ -39,11 +39,43 @@ extension FramingParserTests {
     
     func testSimpleCommand() {
         var buffer: ByteBuffer = "A1 NOOP\r\n"
-        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), ["A1 NOOP \r\n"])
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), ["A1 NOOP\r\n"])
     }
     
     func testSimpleCommandTimes2() {
         var buffer: ByteBuffer = "A1 NOOP\r\nA2 NOOP\r\n"
-        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), ["A1 NOOP \r\n", "A2 NOOP \r\n"])
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), ["A1 NOOP\r\n", "A2 NOOP\r\n"])
+    }
+    
+    // Note that we don't jump the gun when we see a \r, we wait until
+    // we've also examined the next byte to see if we should also have
+    // consumed a \n.
+    func testDripfeeding() {
+        var buffer: ByteBuffer = "A"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = "1"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = " "
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = "N"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = "O"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = "O"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = "P"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = "\r"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), [])
+        
+        buffer = "\n"
+        XCTAssertEqual(self.parser.appendAndFrameBuffer(&buffer), ["A1 NOOP\r\n"])
     }
 }
