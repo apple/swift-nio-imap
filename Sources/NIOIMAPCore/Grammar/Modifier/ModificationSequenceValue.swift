183d26a3b5017ec38a97b889589a1bee457cd46d
Matt Diephouse <diephouse@apple.com>
2021-09-08 09:53:44 -0400

    Make ModificationSequenceValue strideable

--- Sources/NIOIMAPCore/Grammar/Modifier/ModificationSequenceValue.swift
+++ Sources/NIOIMAPCore/Grammar/Modifier/ModificationSequenceValue.swift
@@ -26,6 +26,7 @@ public struct ModificationSequenceValue: Hashable {
     /// Creates a new ModificationSequenceValue.
     /// - parameter value: The raw value.
     public init(_ value: UInt64) {
+        precondition(value <= UInt64(Int64.max), "mod-sequence-values are 63-bit")
         self.value = value
     }
 }
@@ -58,6 +59,40 @@ extension ModificationSequenceValue: CustomDebugStringConvertible {
 extension BinaryInteger {
     public init(_ modificationSequenceValue: ModificationSequenceValue) {
         self = Self(modificationSequenceValue.value)
+    }
+}
+
+// MARK: - Strideable
+
+extension ModificationSequenceValue: Strideable {
+    /// Evaluates if one `ModificationSequenceValue` (`lhs`) is strictly less than another (`rhs`).
+    /// - parameter lhs: The first `ModificationSequenceValue` to evaluate.
+    /// - parameter rhs: The second `ModificationSequenceValue` to evaluate.
+    /// - returns: `true` if `lhs` strictly less than`rhs`, otherwise `false`.
+    public static func < (lhs: ModificationSequenceValue, rhs: ModificationSequenceValue) -> Bool {
+        lhs.value < rhs.value
+    }
+
+    /// Evaluates if one `ModificationSequenceValue` (`lhs`) is less than or equal to another (`rhs`).
+    /// - parameter lhs: The first `ModificationSequenceValue` to evaluate.
+    /// - parameter rhs: The second `ModificationSequenceValue` to evaluate.
+    /// - returns: `true` if `lhs` is less than or equal to `rhs`, otherwise `false`.
+    public static func <= (lhs: ModificationSequenceValue, rhs: ModificationSequenceValue) -> Bool {
+        lhs.value <= rhs.value
+    }
+
+    /// Gets the distance to the given `ModificationSequenceValue`.
+    /// - parameter other: The `ModificationSequenceValue` to get the distance to.
+    /// - returns: The distance.
+    public func distance(to other: ModificationSequenceValue) -> Int64 {
+        Int64(other.value) - Int64(self.value)
+    }
+
+    /// Advances the current ModificationSequenceValue by `n`.
+    /// - parameter n: How many to advance by.
+    /// - returns: A new `ModificationSequenceValue`.
+    public func advanced(by n: Int64) -> ModificationSequenceValue {
+        return ModificationSequenceValue(UInt64(Int64(self.value) + n))
     }
 }
 
