6648abfd88b83ce49b79434b0ada4b20d46edd44
David Evans <d.evans@apple.com>
2021-11-30 10:59:41 +0000

    Update all docs to correctly represent types
    Due to all the changes to create generic types a lot of the types specified in various bits of documentation were actually wrong. They've been fixed.

--- Sources/NIOIMAPCore/Grammar/UID/UID.swift
+++ Sources/NIOIMAPCore/Grammar/UID/UID.swift
@@ -19,17 +19,17 @@ public protocol MessageIdentifier: Hashable, Codable, CustomDebugStringConvertib
 }
 
 extension MessageIdentifier {
-    /// The minimum `UID` is always *1*.
+    /// The minimum `MessageIdentifier` is always *1*.
     public static var min: Self {
         self.init(rawValue: 1)
     }
 
-    /// The maximum `UID` is always `UInt32.max`.
+    /// The maximum `MessageIdentifier` is always `UInt32.max`.
     public static var max: Self {
         self.init(rawValue: UInt32.max)
     }
 
-    /// Creates a `UID` from some `BinaryInteger`, ensuring that the given value fits within a `UInt32`.
+    /// Creates a `MessageIdentifier` from some `BinaryInteger`, ensuring that the given value fits within a `UInt32`.
     /// - parameter source: The raw value to use.
     /// - returns: `nil` if `source` does not fit within a `UInt32`, otherwise a `UID`.
     public init?<T>(exactly source: T) where T: BinaryInteger {
@@ -42,7 +42,7 @@ extension MessageIdentifier {
         self.init(exactly: wrapper.rawValue + 1)!
     }
 
-    /// Creates a human-readable `String` representation of the `UID`.
+    /// Creates a human-readable `String` representation of the `MessageIdentifier`.
     /// `*` if `self = UInt32.max`, otherwise `self.rawValue` as a `String`.
     public var debugDescription: String {
         if self == .max {
@@ -52,7 +52,7 @@ extension MessageIdentifier {
         }
     }
 
-    /// Creates a new `UID` from an integer literal, skipping all validation.
+    /// Creates a new `MessageIdentifier` from an integer literal, skipping all validation.
     /// - parameter integerLiteral: The integer literal value.
     public init(integerLiteral value: UInt32) {
         assert(value >= 1)
@@ -62,11 +62,11 @@ extension MessageIdentifier {
 
 /// Unique Message Identifier
 ///
-/// Not that valid UIDs are 1 ... 4294967295 (UInt32.max).
+/// Not that valid `MessageIdentifier`s are 1 ... 4294967295 (UInt32.max).
 /// The maximum value is often rendered as `*` when encoded.
 ///
 /// See RFC 3501 section 2.3.1.1.
-public struct UID: MessageIdentifier {
+public struct MessageIdentifier: MessageIdentifier {
     public var rawValue: UInt32
 
     public init(rawValue: UInt32) {
@@ -83,34 +83,34 @@ extension BinaryInteger {
 // MARK: - Strideable
 
 extension MessageIdentifier {
-    /// Evaluates if one `UID` (`lhs`) is strictly less than another (`rhs`).
-    /// - parameter lhs: The first `UID` to evaluate.
-    /// - parameter rhs: The second `UID` to evaluate.
+    /// Evaluates if one `MessageIdentifier` (`lhs`) is strictly less than another (`rhs`).
+    /// - parameter lhs: The first `MessageIdentifier` to evaluate.
+    /// - parameter rhs: The second `MessageIdentifier` to evaluate.
     /// - returns: `true` if `lhs` strictly less than`rhs`, otherwise `false`.
     public static func < (lhs: Self, rhs: Self) -> Bool {
         lhs.rawValue < rhs.rawValue
     }
 
-    /// Evaluates if one `UID` (`lhs`) is less than or equal to another (`rhs`).
-    /// - parameter lhs: The first `UID` to evaluate.
-    /// - parameter rhs: The second `UID` to evaluate.
+    /// Evaluates if one `MessageIdentifier` (`lhs`) is less than or equal to another (`rhs`).
+    /// - parameter lhs: The first `MessageIdentifier` to evaluate.
+    /// - parameter rhs: The second `MessageIdentifier` to evaluate.
     /// - returns: `true` if `lhs` is less than or equal to `rhs`, otherwise `false`.
     public static func <= (lhs: Self, rhs: Self) -> Bool {
         lhs.rawValue <= rhs.rawValue
     }
 
-    /// Gets the distance to the given `UID`.
-    /// - parameter other: The `UID` to get the distance to.
+    /// Gets the distance to the given `MessageIdentifier`.
+    /// - parameter other: The `MessageIdentifier` to get the distance to.
     /// - returns: The distance.
     public func distance(to other: Self) -> Int64 {
         Int64(other.rawValue) - Int64(self.rawValue)
     }
 
-    /// Advances the current UID by `n`.
+    /// Advances the current `MessageIdentifier` by `n`.
     /// IMPORTANT: `n` *must* be `<= UInt32.max`. `Int64` is used as the stridable type as it allows
     /// values equal to `UInt32.max` on all platforms (including 32 bit platforms where `Int.max < UInt32.max`.
     /// - parameter n: How many to advance by.
-    /// - returns: A new `UID`.
+    /// - returns: A new `MessageIdentifier`.
     public func advanced(by n: Int64) -> Self {
         precondition(n <= UInt32.max, "`n` must be less than UInt32.max")
         return Self(exactly: Int64(self.rawValue) + n)!
@@ -120,14 +120,6 @@ extension MessageIdentifier {
 // MARK: - Encoding
 
 extension EncodeBuffer {
-    @discardableResult mutating func writeUID(_ num: UID) -> Int {
-        if num == .max {
-            return self.writeString("*")
-        } else {
-            return self.writeString("\(num.rawValue)")
-        }
-    }
-
     @discardableResult mutating func writeMessageIdentifier<IdentifierType: MessageIdentifier>(_ id: IdentifierType) -> Int {
         if id == .max {
             return self.writeString("*")
