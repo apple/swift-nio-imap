b4e2d0f797d3605fad947c09f27a627c6c0ad1ec
David Evans <d.evans@apple.com>
2021-12-13 14:04:27 +0000

    Add conversions

---
+++ Tests/NIOIMAPCoreTests/Grammar/MessageIdentifierRange+Tests.swift
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIO
+@_spi(NIOIMAPInternal) @testable import NIOIMAPCore
+import XCTest
+
+class MessageIdentifierRange_Tests: EncodeTestClass {}
+
+// MARK: - Conversion
+
+extension MessageIdentifierRange_Tests {
+    func testConvert_sequenceNumber() {
+        let input = MessageIdentifierRange<UnknownMessageIdentifier>(1 ... 2)
+        let output = MessageIdentifierRange<SequenceNumber>(input)
+        XCTAssertEqual(output, 1...2)
+    }
+    
+    func testConvert_uid() {
+        let input = MessageIdentifierRange<UnknownMessageIdentifier>(5 ... 6)
+        let output = MessageIdentifierRange<UID>(input)
+        XCTAssertEqual(output, 5...6)
+    }
+    
+}
---
+++ Tests/NIOIMAPCoreTests/Grammar/MessageIdentifierSet+Tests.swift
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIO
+@_spi(NIOIMAPInternal) @testable import NIOIMAPCore
+import XCTest
+
+class MessageIdentifierSet_Tests: EncodeTestClass {}
+
+// MARK: - Conversion
+
+extension MessageIdentifierSet_Tests {
+    func testConvert_sequenceNumber() {
+        let input = MessageIdentifierSet<UnknownMessageIdentifier>([1...5, 10...15, 20...30])
+        let output = MessageIdentifierSet<SequenceNumber>(input)
+        XCTAssertEqual(output, [1...5, 10...15, 20...30])
+    }
+    
+    func testConvert_uid() {
+        let input = MessageIdentifierSet<UnknownMessageIdentifier>([1...5, 10...15, 20...30])
+        let output = MessageIdentifierSet<UID>(input)
+        XCTAssertEqual(output, [1...5, 10...15, 20...30])
+    }
+    
+}
