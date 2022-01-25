3aba2ac87ad9879f543de182d8aef9879db637e4
David Evans <d.evans@apple.com>
2022-01-24 22:53:40 -0800

    Fix merge (maybe)

--- Sources/NIOIMAPCore/Parser/ResponseParser.swift
+++ Sources/NIOIMAPCore/Parser/ResponseParser.swift
@@ -49,11 +49,16 @@ public struct ResponseParser: Parser {
 
     /// Creates a new `ResponseParser`.
     /// - parameter bufferLimit: The maximum amount of data that may be buffered by the parser. If this limit is exceeded then an error will be thrown. Defaults to 1000 bytes.
-    public init(bufferLimit: Int = 8_192, messageAttributeLimit: Int = .max, bodySizeLimit: UInt64 = .max, parsedStringCache: ((String) -> String)? = nil) {
+    public init(
+        bufferLimit: Int = 8_192,
+        messageAttributeLimit: Int = .max,
+        bodySizeLimit: UInt64 = .max,
+        literalSizeLimit: Int = IMAPDefaults.literalSizeLimit,
+        parsedStringCache: ((String) -> String)? = nil
+    ) {
         self.bufferLimit = bufferLimit
         self.mode = .response(.fetchOrNormal)
         self.messageAttributeLimit = messageAttributeLimit
-        self.literalSizeLimit = .max
         self.parser = GrammarParser(literalSizeLimit: literalSizeLimit, parsedStringCache: parsedStringCache)
         self.bodySizeLimit = bodySizeLimit
     }
