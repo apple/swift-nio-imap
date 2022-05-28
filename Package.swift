e54ba8f873c3cd375e26755102cd7d9638434ceb
David Evans <d.evans@apple.com>
2022-05-28 11:10:02 +0100

    Update to Swift 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,8 +11,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio", from: "2.38.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl", from: "2.17.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.0"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", .exact("0.49.4")),
-        .package(url: "https://github.com/apple/swift-standard-library-preview.git", .exact("0.0.3")),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.49.4"),
+        .package(url: "https://github.com/apple/swift-standard-library-preview.git", exact: "0.0.3"),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.2")),
     ],
     targets: [
