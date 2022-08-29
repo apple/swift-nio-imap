4409e0e5e3c1be9dc9bf063ecde6ee69679b2e80
Daniel Eggert <deggert@apple.com>
2022-08-29 20:15:24 +0200

    Change dependencies to not used a fixed version.

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio", from: "2.40.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl", from: "2.20.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.0"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.49.11"),
-        .package(url: "https://github.com/apple/swift-standard-library-preview.git", exact: "0.0.3"),
-        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.2")),
+        .package(url: "https://github.com/apple/swift-standard-library-preview.git", from: "0.0.3"),
+        .package(url: "https://github.com/apple/swift-collections.git", "1.0.2" ..< "2.0.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.11"),
     ],
     targets: [
         .executableTarget(
