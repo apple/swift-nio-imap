6eb8a70b2c1e10f44d9b17777c0527e7cae80eb8
Daniel Eggert <deggert@apple.com>
2022-10-04 17:58:35 +0200

    More BodyStructure / SectionSpec convenience (#715)

--- Tests/NIOIMAPCoreTests/Grammar/Body/BodyStructure+Tests.swift
+++ Tests/NIOIMAPCoreTests/Grammar/Body/BodyStructure+Tests.swift
@@ -313,4 +313,161 @@ extension BodyStructure_Tests {
             XCTAssertEqual(input[index], expected, line: line)
         }
     }
+
+    func testEnueratingParts() {
+        let inputs: [(BodyStructure, [(SectionSpecifier.Part, BodyStructure)], UInt)] = [
+            (
+                .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                [
+                    (
+                        [],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0)))
+                    ),
+                ],
+                #line
+            ),
+            (
+                .multipart(.init(parts: [
+                    .singlepart(.init(
+                        kind: .message(BodyStructure.Singlepart.Message(
+                            message: .rfc822,
+                            envelope: Envelope(date: nil, subject: "A", from: [], sender: [], reply: [], to: [], cc: [], bcc: [], inReplyTo: nil, messageID: nil),
+                            body: .multipart(.init(parts: [
+                                .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 1))),
+                                .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 2))),
+                            ], mediaSubtype: .init("mixed"))),
+                            lineCount: 321
+                        )),
+                        fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0)
+                    )),
+                    .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                ], mediaSubtype: .init("mixed"))),
+                [
+                    (
+                        [],
+                        .multipart(.init(parts: [
+                            .singlepart(.init(
+                                kind: .message(BodyStructure.Singlepart.Message(
+                                    message: .rfc822,
+                                    envelope: Envelope(date: nil, subject: "A", from: [], sender: [], reply: [], to: [], cc: [], bcc: [], inReplyTo: nil, messageID: nil),
+                                    body: .multipart(.init(parts: [
+                                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 1))),
+                                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 2))),
+                                    ], mediaSubtype: .init("mixed"))),
+                                    lineCount: 321
+                                )),
+                                fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0)
+                            )),
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                        ], mediaSubtype: .init("mixed")))
+                    ),
+                    (
+                        [1],
+                        .singlepart(.init(
+                            kind: .message(BodyStructure.Singlepart.Message(
+                                message: .rfc822,
+                                envelope: Envelope(date: nil, subject: "A", from: [], sender: [], reply: [], to: [], cc: [], bcc: [], inReplyTo: nil, messageID: nil),
+                                body: .multipart(.init(parts: [
+                                    .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 1))),
+                                    .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 2))),
+                                ], mediaSubtype: .init("mixed"))),
+                                lineCount: 321
+                            )),
+                            fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0)
+                        ))
+                    ),
+                    (
+                        [1, 1],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 1)))
+                    ),
+                    (
+                        [1, 2],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 2)))
+                    ),
+                    (
+                        [2],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0)))
+                    ),
+                ],
+                #line
+            ),
+            (
+                .multipart(.init(parts: [
+                    .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                    .multipart(.init(parts: [
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                        .multipart(.init(parts: [
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 3))),
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 4))),
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 5))),
+                        ], mediaSubtype: .init("subtype"))),
+                    ], mediaSubtype: .init("subtype"))),
+                ], mediaSubtype: .init("subtype"))),
+                [
+                    (
+                        [],
+                        .multipart(.init(parts: [
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                            .multipart(.init(parts: [
+                                .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                                .multipart(.init(parts: [
+                                    .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 3))),
+                                    .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 4))),
+                                    .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 5))),
+                                ], mediaSubtype: .init("subtype"))),
+                            ], mediaSubtype: .init("subtype"))),
+                        ], mediaSubtype: .init("subtype")))
+                    ),
+                    (
+                        [1],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0)))
+                    ),
+                    (
+                        [2],
+                        .multipart(.init(parts: [
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0))),
+                            .multipart(.init(parts: [
+                                .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 3))),
+                                .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 4))),
+                                .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 5))),
+                            ], mediaSubtype: .init("subtype"))),
+                        ], mediaSubtype: .init("subtype")))
+                    ),
+                    (
+                        [2, 1],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 0)))
+                    ),
+                    (
+                        [2, 2],
+                        .multipart(.init(parts: [
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 3))),
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 4))),
+                            .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 5))),
+                        ], mediaSubtype: .init("subtype")))
+                    ),
+                    (
+                        [2, 2, 1],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 3)))
+                    ),
+                    (
+                        [2, 2, 2],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 4)))
+                    ),
+                    (
+                        [2, 2, 3],
+                        .singlepart(.init(kind: .basic(.init(kind: .audio, subtype: .alternative)), fields: .init(parameters: [:], id: nil, contentDescription: nil, encoding: .binary, octetCount: 5)))
+                    ),
+                ],
+                #line
+            ),
+        ]
+        inputs.forEach { (input, expected, line) in
+            var result: [(SectionSpecifier.Part, BodyStructure)] = []
+            input.enumerateParts {
+                result.append(($0, $1))
+            }
+            XCTAssertEqual(result.map(\.0), expected.map(\.0), line: line)
+            XCTAssertEqual(result.map(\.1), expected.map(\.1), line: line)
+        }
+    }
 }
