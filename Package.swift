9ff669e157494c1596262a148025cf42c0350b9b
Matt Diephouse <diephouse@apple.com>
2024-08-28 10:49:58 -0400

    Update swift-collections to 1.1.0+

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-ssl", from: "2.24.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.4"),
         .package(url: "https://github.com/apple/swift-se0270-range-set.git", from: "1.0.1"),
-        .package(url: "https://github.com/apple/swift-collections.git", "1.0.2" ..< "2.0.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", "1.1.0" ..< "2.0.0"),
     ],
     targets: [
         .executableTarget(
