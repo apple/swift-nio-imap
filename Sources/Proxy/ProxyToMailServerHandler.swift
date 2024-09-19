e97b03ca5c2608fae9b77fc80e8c7071cd68e3b0
Aloisius <Aloisius@users.noreply.github.com>
2024-09-19 07:58:59 -0700

    Fix IMAP Proxy channel pipeline handlers - fixes #764 (#765)

--- Sources/Proxy/ProxyToMailServerHandler.swift
+++ Sources/Proxy/ProxyToMailServerHandler.swift
@@ -36,10 +36,7 @@ class ProxyToMailServerHandler: ChannelInboundHandler {
 
     func channelRead(context: ChannelHandlerContext, data: NIOAny) {
         let stream = self.unwrapInboundIn(data)
-        let buffer = context.channel.allocator.buffer(capacity: 1024)
-        var encodeBuffer = ResponseEncodeBuffer(buffer: buffer, capabilities: self.capabilities, loggingMode: false)
-        encodeBuffer.writeResponse(stream)
-        self.mailAppToProxyChannel.writeAndFlush(encodeBuffer.readBytes(), promise: nil)
+        self.mailAppToProxyChannel.writeAndFlush(stream, promise: nil)
     }
 
     func errorCaught(context: ChannelHandlerContext, error: Error) {
