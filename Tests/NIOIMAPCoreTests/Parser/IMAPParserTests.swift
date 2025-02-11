7f0c7d125a919577183bbd03e2ab84a74105fca7
Matt Diephouse <diephouse@apple.com>
2025-02-11 11:09:44 -0500

    Parse `MailboxAttribute.mailboxID`

--- Tests/NIOIMAPCoreTests/Parser/IMAPParserTests.swift
+++ Tests/NIOIMAPCoreTests/Parser/IMAPParserTests.swift
@@ -2760,6 +2760,7 @@ extension ParserUnitTests {
                     .init(nextUID: 95604, uidValidity: 33682, highestModificationSequence: 35227, appendLimit: 81818),
                     #line
                 ),
+                ("MAILBOXID (F2212ea87-6097-4256-9d51-71338625)", ")", .init(mailboxID: "F2212ea87-6097-4256-9d51-71338625"), #line),
             ],
             parserErrorInputs: [
                 ("MESSAGES UNSEEN 3 RECENT 4", "\r", #line),
