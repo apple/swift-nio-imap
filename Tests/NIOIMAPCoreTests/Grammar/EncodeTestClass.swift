1ec44190e9d5ca1af7960a40f82febf5aff993d1
Daniel Eggert <deggert@apple.com>
2025-06-11 16:24:06 +0200

    Whitespace

--- Tests/NIOIMAPCoreTests/Grammar/EncodeTestClass.swift
+++ Tests/NIOIMAPCoreTests/Grammar/EncodeTestClass.swift
@@ -86,7 +86,11 @@ class EncodeTestClass: XCTestCase {
             do {
                 self.testBuffer.mode = .client(options: options)
                 let size = try encoder(test)
-                XCTAssertEqual(size, expectedStrings.reduce(0) { $0 + $1.utf8.count }, file: (file), line: line)
+                XCTAssertEqual(
+                    size,
+                    expectedStrings.reduce(0) { $0 + $1.utf8.count },
+                    file: (file), line: line
+                )
                 XCTAssertEqual(self.testBufferStrings, expectedStrings, file: (file), line: line)
             } catch {
                 XCTFail("\(error)", file: (file), line: line)
